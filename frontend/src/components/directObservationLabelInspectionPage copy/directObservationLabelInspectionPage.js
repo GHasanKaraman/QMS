a561d03991a85d2c80c8ef956b599c621b5c3896
Gurkan <hasankaraman_tr@hotmail.com>
2024-02-28 15:04:17 -0500

    response check with status codes

--- frontend/src/components/directObservationLabelInspectionPage copy/directObservationLabelInspectionPage.js
+++ frontend/src/components/directObservationLabelInspectionPage copy/directObservationLabelInspectionPage.js
@@ -148,10 +148,38 @@ const DirectObservationLabelInspectionPage = (props) => {
     loadOperators();
   }, []);
 
-  const handleSubmit = async (values) => {
+  const handleSubmit = async (values, { resetForm }) => {
     setOpen(true);
     values.product = values.product.partNum;
     const res = await axios.post("/labelinspection/add", values);
+    if (userAuth.control(res)) {
+      if (res?.data) {
+        enqueueSnackbar("You have successfully created the form!", {
+          variant: "success",
+        });
+        resetForm();
+      } else {
+        switch (res.response?.status) {
+          case 404:
+            enqueueSnackbar("Station or product is wrong!", {
+              variant: "error",
+            });
+            break;
+          case 503:
+            enqueueSnackbar("Something went wrong with the server!", {
+              variant: "error",
+            });
+            break;
+        }
+      }
+    } else {
+      navigate("/login");
+      localStorage.removeItem("token");
+      localStorage.removeItem("username");
+      enqueueSnackbar("Please sign in again!", {
+        variant: "error",
+      });
+    }
     setOpen(false);
   };
 
