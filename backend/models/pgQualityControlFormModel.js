118450e2dbda851628f599981bb5bf50b45c6bea
Gurkan <hasankaraman_tr@hotmail.com>
2024-03-13 11:03:56 -0400

    p&g form can be saved to db

---
+++ backend/models/pgQualityControlFormModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const pgQualityControlFormSchema = new mongoose.Schema(
+  {
+    station: { type: String, required: true },
+    product: { type: String, required: true },
+    username: { type: String, required: true },
+    lotCodePouch: { type: String, required: true },
+    expirationDatePouch: { type: String, required: true },
+    currentWeightPouch: { type: String, required: true },
+    isNotchCorrect: { type: String, required: true },
+    isSealCorrectPouch: { type: String, required: true },
+    metalDetector: { type: String, required: true },
+    currentWeightBox: { type: String, required: true },
+    isSealCorrectBox: { type: String, required: true },
+    expirationDateCase: { type: String, required: true },
+    lotCodeCase: { type: String, required: true },
+    imageIDs: [{ type: mongoose.Types.ObjectId }],
+    status: { type: String, default: "failed" },
+    signedOff: { type: String, default: "" },
+    signOffDate: { type: Date },
+  },
+  { timestamps: true }
+);
+
+const pgQualityControlFormModel = mongoose.model(
+  "pgQualityControlForms",
+  pgQualityControlFormSchema
+);
+
+module.exports = pgQualityControlFormModel;
