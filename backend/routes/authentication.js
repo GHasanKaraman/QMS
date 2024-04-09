98e4cf78bd5f01a04692451704e36a85c6058051
Gurkan <hasankaraman_tr@hotmail.com>
2024-04-09 13:17:48 -0400

    node-fetch installed

--- backend/routes/authentication.js
+++ backend/routes/authentication.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const md5 = require("md5");
+const fetch = require("node-fetch");
 const uuid = require("uuid");
 
 const tokenModel = require("../models/tokenModel");
