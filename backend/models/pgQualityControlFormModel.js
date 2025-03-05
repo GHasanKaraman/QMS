6027642f46f42ecbe4342e1bd2f8bfefd9518ec2
GHasanKaraman <40955470+GHasanKaraman@users.noreply.github.com>
2025-03-05 14:06:58 -0500

    Update pgQualityControlFormModel.js

--- backend/models/pgQualityControlFormModel.js
+++ backend/models/pgQualityControlFormModel.js
@@ -8,7 +8,6 @@ const pgQualityControlFormSchema = new mongoose.Schema(
     metalBallSsDetected: { type: String, default: "" },
     areIngredientsCorrect: { type: String, required: true },
     station: { type: String, required: true },
-    shift: { type: String, required: true },
     product: { type: String, required: true },
     username: { type: String, required: true },
     lotCodePouch: { type: String, required: true },
