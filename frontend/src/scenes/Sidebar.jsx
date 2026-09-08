95c6f7a97d2b3bf62bc8c01c83d709caafcd6320
GHasanKaraman <40955470+GHasanKaraman@users.noreply.github.com>
2026-09-08 17:45:12 -0400

    new version number

--- frontend/src/scenes/Sidebar.jsx
+++ frontend/src/scenes/Sidebar.jsx
@@ -167,7 +167,7 @@ const Sidebar = () => {
             />
             {!isCollapsed ? (
               <Typography variant="h6" color={colors.grey[100]}>
-                v1.0.10
+                v1.0.11
               </Typography>
             ) : null}
           </div>
